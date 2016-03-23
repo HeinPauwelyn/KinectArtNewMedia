78d134764170c33bff100bb5914cea3365749eab
Jonas Vanhecke <otakujonas@gmail.com>
2016-03-23 17:32:18 +0100

    C#

--- KinectArt.CSharp/KinectArt.CSharp.App/MainWindow.xaml.cs
+++ KinectArt.CSharp/KinectArt.CSharp.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,73 @@ namespace KinectArt.CSharp.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private KinectSensor sensor;
+        private WriteableBitmap colorBitmap;
+
         public MainWindow()
         {
             InitializeComponent();
-            //Hey
+            Loaded += WindowLoaded;
+            Closed += WindowClosed;
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            sensor?.Stop();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            LoadKinect();
+        }
+
+        private void LoadKinect()
+        {
+            foreach (KinectSensor potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if(sensor != null)
+            {
+                //Enable'n
+                sensor.DepthStream.Enable();
+                sensor.SkeletonStream.Enable();
+                sensor.ColorStream.Enable();
+
+                colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Gray16, null);
+
+                Image.Source = colorBitmap;
+
+                //Event aanmaken
+                sensor.AllFramesReady += Sensor_AllFramesReady;
+
+                //Starten
+                sensor.Start();
+            }
+        }
+
+        private void Sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+colorFrame.CopyPixelDataTo(new byte[this.sensor.ColorStream.FramePixelDataLength]);
+
+                    // Write the pixel data into our bitmap
+                    colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        new byte[this.sensor.ColorStream.FramePixelDataLength],
+                        this.colorBitmap.PixelWidth * colorFrame.BytesPerPixel,
+                        0);
+                }
+            }
         }
     }
 }
--- project/project.pde
+++ project/project.pde
@@ -1,9 +1,30 @@
-void setup(){
-	size(50,50);
-	//Test
-	//Comment 2
-	//Test 2
+import SimpleOpenNI.*;
+SimpleOpenNI kinect;
+
+color[] colors = { #E9D33F, #D1A72C, #42919F, #FE3830, #F28425 };
+
+void setup() {
+	size(640, 480);
+	kinect = new SimpleOpenNI(this);
+
+	kinect.enableDepth();
+	kinect.enableRGB();
 }
 
-void draw(){
+void draw() {
+	kinect.update();
+	image(kinect.depthImage(), 0, 0);
+	image(kinect.rgbImage(), 640, 0);
+
+	DrawLines();
+	//delay(1000);
+}
+
+void DrawLines() {
+	for(int i = 0; i <= 100; i++) {
+    	stroke(colors[int(random(colors.length))]);
+    	strokeWeight(2);
+
+        line(random(width), random(height), random(width), random(height));
+    }
 }
